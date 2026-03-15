535cbb78f7de6410083a0e4a0efffaedac19c05b
eros-baez <baezerosdaniel@gmail.com>
2026-03-14 23:34:32 -0300

    ex n 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please enter your grade percentage: ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 93)
+        {
+            Console.WriteLine("Your calification is: A");
+        }
+
+        if (grade < 93 && grade >= 90)
+        {
+            Console.WriteLine("Your calification is: A-");
+        }
+
+        if (grade < 90 && grade >= 87)
+        {
+            Console.WriteLine("Your calification is: B+");
+        }
+        if (grade < 87 && grade >= 83)
+        {
+            Console.WriteLine("Your calification is: B");
+        }
+        if (grade < 83 && grade >= 80)
+        {
+            Console.WriteLine("Your calification is: B-");
+        }
+        if (grade < 80 && grade >= 77)
+        {
+            Console.WriteLine("Your calification is: C+");
+        }
+        if (grade < 77 && grade >= 73)
+        {
+            Console.WriteLine("Your calification is: C");
+        }
+        if (grade < 73 && grade >= 70)
+        {
+            Console.WriteLine("Your calification is: C-");
+        }
+        if (grade < 70 && grade >= 67)
+        {
+            Console.WriteLine("Your calification is: D+");
+        }
+        if (grade < 67 && grade >= 63)
+        {
+            Console.WriteLine("Your calification is: D");
+        }
+        if (grade < 63 && grade >= 60)
+        {
+            Console.WriteLine("Your calification is: D-");
+        }
+        if (grade < 60)
+        {
+            Console.WriteLine("Your calification is: F");
+        }
+
     }
 }
