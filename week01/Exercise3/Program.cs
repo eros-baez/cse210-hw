80ac6ad9e598f013f9a5db518ae58bd21bcaefd8
eros-baez <baezerosdaniel@gmail.com>
2026-03-18 09:07:50 -0300

    Ex 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string continuePlaying = "yes";
+
+        while (continuePlaying == "yes")
+            {
+                Random generator = new Random();
+                int magicNumber = generator.Next(1, 101);
+                int guess = -1;
+                int attempts = 0;
+
+
+                while (guess != magicNumber)
+                    {
+                        Console.Write("Guess the number: ");
+                        string input = Console.ReadLine();
+                        guess = int.Parse(input);
+                        attempts = attempts + 1;
+
+                        if (guess > magicNumber)
+                        {
+                            Console.WriteLine("Lower");
+                        }
+                        if (guess < magicNumber)
+                        {
+                            Console.WriteLine("Higher");
+                        }
+                    }
+
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You have taken {attempts} attempts ");
+
+
+                Console.Write("Do you want to play again?(yes/no): ");
+                continuePlaying = Console.ReadLine();
+            }
+         
+
     }
 }
