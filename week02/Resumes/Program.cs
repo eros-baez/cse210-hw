a2e50a77f32caabe9b8711172e92b5f4ce7fc67e
eros-baez <baezerosdaniel@gmail.com>
2026-03-18 12:39:37 -0300

    resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2010;
+        job1._endYear = 2020;
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2020;
+        job2._endYear = 2026;
+        job2.Display();
+
+        Console.WriteLine("");
+
+        Resume resume = new Resume();
+        resume._name = "Eros Baez";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        resume.Display();
     }
 }
